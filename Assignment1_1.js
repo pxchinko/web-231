0771c7dfb9417778433e6eb3ad85f5ba3603422f
Sarah Massie <smassie@my365.bellevue.edu>
2020-05-27 19:53:00 -0700

    Add files via upload

---
+++ Assignment1_1.js
@@ -0,0 +1,16 @@
+/*
+*Title: Assignment 1.1
+*Author: Professor Krasso
+*Date: 27 May 2020
+*Modified By: Sarah Massie
+*Description: First JavaScript with at least two errors.
+*/
+
+// 1 error below
+var msg = 'Hello class'
+console.log(message)
+
+//3 errors bellow
+var GameTitle = 'Animal Crossing'
+var GameSystem = 'Nintendo Switch'
+consolelog(GameSystem)
