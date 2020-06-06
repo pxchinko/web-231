7ec4cb3955888bf05910810650e2d7b6afb4f80f
Sarah Massie <smassie@my365.bellevue.edu>
2020-06-05 17:48:33 -0700

    Add files via upload

--- week-2/directory/Massie-header-test.js
+++ week-2/directory/Massie-header-test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 ============================================
 ; Title:  Massie-header-test.js
@@ -20,31 +19,6 @@ exports.display = function (firstName, lastName, assignment) {
 
 	return output  
 } 
-const header = require('./Massie.js');
+const header = require('./Massie-header.js');
 
-=======
-/*
-============================================
-; Title:  Massie-header-test.js
-; Author: Professor Krasso 
-; Date:   5 June 2020
-; Modified By: Sarah Massie
-; Description: Displays a formatted header
-;===========================================
-*/
-
-/**
-* Params: firstName, lastName, assignment
-* Response: output 
-* Description: Returns a well-formatted string header
-*/
-exports.display = function (firstName, lastName, assignment) {
-	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' + 
-    new Date().toLocaleDateString('en-US')
-
-	return output  
-} 
-const header = require('./Massie.js');
-
->>>>>>> bf9283215ff07b401138bb090fc34519cd82e4a0
 console.log(header.display("Sarah", "Massie", "Massie-header-test.js"));
