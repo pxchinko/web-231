b9a66e5cb8e58aa61b3d8aaeaeb32e41e120ba7f
Sarah Massie <pxchinko@gmail.com>
2020-06-10 21:11:34 -0700

    master

--- week-3/directory/Discussion Board 3.1.js
+++ week-3/directory/Discussion Board 3.1.js
@@ -1,3 +1,4 @@
+
 /*=======================================
 ; Title: Discussion Board 3.1
 ; Author: Professor Krasso
@@ -6,23 +7,23 @@
 ; Description: Javascript program using an if else statement with 2 errors.
 =======================================*/
 
-const score = 40;     // Score
+const score >= 70;  // Score
 var msg;            // Message
 
 function congratulate() {
-    msg = 'Congratulations!';
+  msg = 'Congratulations!';
 }
 
 function disappointment() {
-    msg ='Sorry.'
+  msg ='Sorry.';
 }
 
-if (score >= 50) {  // If score is 50 or higher
-    congratulate();
-    msg += ' Proceed to next round.';
-} else (score <= 50) {  // If score is less than 50
-    disappointment();
-    msg += ' Try that again.';
+if (score >= 70) {      // If score is 50 or higher
+  congratulate();
+  msg += ' Proceed to next round.';
+} else (score <= 70) {  // If score is less than 50
+  disappointment();
+  msg += ' Try that again.';
 }
 
 console.log(msg);
