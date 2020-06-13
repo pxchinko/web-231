a9fd0dff5c4617f1568a7e0fce1dbcf2badb91bf
Sarah Massie <pxchinko@gmail.com>
2020-06-12 17:31:38 -0700

    master

---
+++ week-3/directory/Massie-Exercise-3.2.js
@@ -0,0 +1,68 @@
+/*
+============================================
+; Title:  Massie-Exercise-3.2.js
+; Author: Professor Krasso
+; Date:   11 June 2020
+; Modified By: Sarah Massie
+; Description: Matching functions exercise
+;===========================================
+*/
+/*
+  Expected output:
+
+  // output from the if...else blocks
+  Truck and Car do not match!
+  Bike and Bike do match!
+  Four and Three do not match!
+*/
+
+// header
+const header = require('./Massie-header.js');
+console.log(header.display("Sarah", "Massie", "Exercise 3.2.js"));
+
+var test1 = "Truck"
+var test2 = "Car"
+var test3 = "Bike"
+var test4 = "Bike"
+var test5 = "Four"
+var test6 = "Three"
+
+// functions
+function match(string1, string2) {
+  if (string1 === string2){
+    return true; }
+  else {
+    return false;
+  }
+}
+
+function logMismatch(string1, string2) {
+  console.log(string1 + " and " + string2 + " do not match!");
+}
+
+function logMatch(string1, string2) {
+ console.log(string1 + " and " + string2 + " do match!");
+}
+
+// output from the match() function
+console.log(match("A", "B"));
+console.log(match(2, 2));
+
+// Conditional if... else statements including 6 variables
+if (match(test1,test2)) {
+  logMatch(test1, test2); }
+else {
+  logMismatch(test1, test2);
+}
+
+if (match(test3,test4)) {
+  logMatch(test3, test4); }
+else {
+  logMismatch(test3, test4);
+}
+
+if (match(test5,test6)) {
+  logMatch(test5, test6); }
+else {
+  logMismatch(test5, test6);
+}
--- week-3/directory/Massie-header.js
+++ week-3/directory/Massie-header.js
@@ -1,6 +1,6 @@
 /*
 ============================================
-; Title:  Massie-header-test.js
+; Title:  Massie-header.js
 ; Author: Professor Krasso 
 ; Date:   5 June 2020
 ; Modified By: Sarah Massie
@@ -13,13 +13,9 @@
 * Response: output 
 * Description: Returns a well-formatted string header
 */
-
 exports.display = function (firstName, lastName, assignment) {
 	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' + 
-    new Date().toLocaleDateString('en-US')
-    
-  return output  
-} 
-const header = require('./Massie-header.js');
+	new Date().toLocaleDateString('en-US')
 
-console.log(header.display("Sarah", "Massie", "Massie-header-test.js"));
+	return output  
+}
