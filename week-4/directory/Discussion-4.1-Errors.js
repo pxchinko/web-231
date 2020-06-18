82e9dde32d7e2c75466d11f275f0fbad030dc44b
Sarah Massie <pxchinko@gmail.com>
2020-06-17 22:29:47 -0700

    master

---
+++ week-4/directory/Discussion-4.1-Errors.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title:  Discussion-4.1-Errors.js
+; Author: Sarah Massie
+; Date:   17 June 2020
+; Modified by: 
+; Description: Program using an array with three errors
+;===========================================
+*/
+
+//header
+const header = require('./Massie-header.js')
+console.log(header.display('Sarah','Massie','Discussion 4.1'))
+
+//program with three errors
+var vehicles = ["Chevrolet Spark", "Fiat 500", "Nissan Leaf", "Volkswagen Beetle", "Honda Fit"]  
+
+function getCuteCar(array, string) {
+  for var v = 0, v < array.length, v++
+  { if array[v] === string
+      console.log(array[v]);
+  }
+}
+
+for (var x = 0; x < vehicles.length; x++) {
+  console.log(vehicles[v]) ;
+  console.log("Which is the cutest small car?");
+}
+
+getCuteCar(vehicles, "Volkswagen Beetle");
+
+// expected output
+/* Which is the cutest small car? 
+   Volkswagen Beetle. */
