68a1e88a40fb2ae56ddbb1a5000194a1d628cb31
Sarah Massie <pxchinko@gmail.com>
2020-07-08 22:53:06 -0700

    master

--- week-7/directory/exercise-7.2.js
+++ week-7/directory/exercise-7.2.js
@@ -1,13 +1,35 @@
 /*
-  Expected output:
-
-  FirstName LastName
-  Exercise 7.2
-  Today's Date
-
-  1 Thomas Edison Software Engineer
-  2 Benjamin Franklin Programmer
-  3 Nikola Tesla Project Manager
-  4 Charles Babbage Product Manager
-  5 Alexander Bell Business Analyst
+============================================
+; Title:  Assignment-7.3.js
+; Author: Professor Krasso
+; Date:   8 July 2020
+; Modified by: Sarah Massie
+; Description: Creating constructor functions
+;===========================================
 */
+
+//header
+const header = require('./Massie-header.js')
+console.log(header.display('Sarah','Massie','Massie-Assignment-5.4'))
+console.log('')
+
+// employee function
+function Employee(id, first, last, title) {
+  this.idNumber = id;
+  this.firstName = first;
+  this.lastName = last;
+  this.title = title;
+  }
+
+var employees = [
+  new Employee(1, "Thomas", "Edison", "Software Engineer"),
+  new Employee(2, "Benjamin", "Franklin", "Programmer"),
+  new Employee(3, "Nikola", "Tesla", "Project Manager"),
+  new Employee(4, "Charles", "Babbage", "Product Manager"),
+  new Employee(5, "Alexander", "Bell", "Business Analyst"),
+];
+
+// output
+employees.forEach( (employees) => {
+  console.log(employees.idNumber + " " + employees.firstName + " " + employees.lastName + " " + employees.title)
+})
